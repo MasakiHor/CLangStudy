6ae1e3c46aab5cb81ae0cc95a3ca236b65ab24eb
Masaki <horikawamasaki0929@gmail.com>
2025-03-16 23:30:29 +0900

    lecture02

---
+++ ForClass/lecture02/MathWords.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void){
+    printf("Math words.\n\n");
+    //putchar('\n');
+    
+    puts(" sin \t lim \t sup");
+    puts(" sine \t limit \t supremum");
+    return 0;
+}
---
+++ ForClass/lecture02/hello.c
@@ -0,0 +1,13 @@
+/* hello, world
+    puts Version */
+    
+#include<stdio.h>
+    
+// main function
+int main(void){
+    
+    //output for stdout.
+    puts("good morning, world");
+    
+    return 15;//val=0
+}
